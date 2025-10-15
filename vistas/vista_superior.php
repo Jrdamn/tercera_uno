776427d91f22142a8ddd1b4cadd73f4175e8d48f
Jrdamn <psloky5@gmail.com>
2025-10-14 23:58:24 -0600

    Proyecto Tercera

---
+++ vistas/vista_superior.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name ="Tercera 1" content="Proyecto Tercera">
+    <title>Proyecto Tercera</title>
+    <link rel="stylesheet" href="./vistas/css/main.css">
+    
+    
+
+
+
+</head>
+
+
